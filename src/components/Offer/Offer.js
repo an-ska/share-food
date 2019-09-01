c9d0fa54624a7ba0b158808b493cd49dad7f62e0
an_ska <1.anna.piotrowska@gmail.com>
2019-09-01 14:21:02 +0200

    Add possibility to render offer details.

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import './Offer.css';
 
-const Offer = ({ title }) => (
-  <div>
-    <h2 className="offer-title">{title}</h2>
+const Offer = ({
+  title,
+  description,
+  soldPortions,
+  availablePortions,
+  portionPrice,
+  authorName
+}) => (
+  <div className="offer">
+    <div className="offer-body">
+      <h2 className="offer-title">{title}</h2>
+      <p className="offer-description">{description}</p>
+      <div className="offer-portion">
+        <span className="offer-portion__counter">{soldPortions}/{availablePortions}</span>
+        <span className="offer-portion__label">portions available</span>
+        <p className="offer-portion__price">{portionPrice} PLN/portion</p>
+        <p className="offer-authorName">offered by {authorName}</p>
+      </div>
+    </div>
   </div>
 );
 
