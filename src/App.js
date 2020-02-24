03072dc93f93257ed852eb8dbe090689d378a70d
an-ska <1.anna.piotrowska@gmail.com>
2020-02-24 21:26:35 +0100

    Add initial routing.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { Switch, Route, Redirect} from "react-router-dom";
 import "./App.css";
 import OfferWall from "./containers/OfferWall/OfferWall";
 import AddOffer from "./containers/AddOffer/AddOffer";
 
 const App = () => {
+    let routes = (
+        <Switch>
+            <Route path="/offers" component={OfferWall} />
+            <Route path="/add-offer" component={AddOffer} />
+            <Redirect to="/offers" />
+        </Switch>
+    );
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <OfferWall />
-                <AddOffer />
-            </header>
-        </div>
+        <div className="App">{routes}</div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const app = (
+    <Router>
+        <App />
+    </Router>
+)
+
+ReactDOM.render(app, document.getElementById("root"));
 
 serviceWorker.unregister();
